64e513db80267daffadb5387f462bc2804d321c8
samisaleem395 <154269549+samisaleem395@users.noreply.github.com>
2024-01-21 00:41:04 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+var age = 19; 
+var isStudent = true;
+
+
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+
+var welcomeMessage = "Welcome! Thanks for visiting.\n";
+welcomeMessage += "I am " + age + " years old, and I am ";
+welcomeMessage += isStudent ? "a student." : "not a student.";
+
+
+
+var length = 15 ;
+var widht = 10 ;
+
+var area = length * widht;
+
+console.log("Area of the rectangular:",area)
+
+var username = prompt("what is your name?");
+
+
+alert("hello, " + username + "! welcome to our website.");
